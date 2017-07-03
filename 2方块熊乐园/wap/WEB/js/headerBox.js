5c60d70672628e74577ad0445e7db5232ddadca1
chaojie <604972395@qq.com>
2017-07-04 01:20:11 +0800

    task

--- 2方块熊乐园/wap/WEB/js/headerBox.js
+++ 2方块熊乐园/wap/WEB/js/headerBox.js
@@ -67,7 +67,9 @@ $(function(){
             if (data.status == 200) {
                 data = JSON.parse(data.data);
                 console.log("获得区域信息", data);
+                var areaIdArr = [];
                 var changeLiStr = '';
+                var areaId = '';
                 for (var areas in data) {
                     //console.log("data[areas].name="+data[areas]);
 
@@ -76,7 +78,52 @@ $(function(){
                      *
                      * .headerBox .header .changeUl .changeLi*/
                     changeLiStr += '<li class="changeLi"><a href="#" class="changeLiLink">'+ data[areas].name +'</a></li>';
+                    areaIdArr.push(data[areas].id);
                 }
+
+                //获取附近商品信息
+                console.log("areaId", areaIdArr);
+                localStorage.setItem('areaId',areaIdArr[0]);
+
+                areaId = localStorage.getItem("areaId");
+                console.log(areaId);
+                areaId = areaId.toString();
+                $.ajax({
+                    type: "get",
+                    url: baseUrl+'/apigateway/getgoods?areaid='+'\"'+areaId+'\"',
+                    success:function(data){
+                        console.log(typeof areaId);
+                        if(data.status == 200){
+                            data = JSON.parse(data.data);
+                            console.log('获取附近商品信息',data);
+                            $('.nearbyImgBox .nearbyImg').empty();
+                            for (var nearby in data){
+                                $('.nearbyImgBox .nearbyImg').append('' +
+                                    '<li class="nearbyLi">' +
+                                    '<img class="goodImg" src='+baseUrl+'/gimg/'+data[nearby].imgs+'>' +
+                                    '<div class="desc row">' +
+                                    '<div class="col-xs-6 col-sm-6 left">'+data[nearby].title+'</div>' +
+                                    '<div class="col-xs-6 col-sm-6 right">'+data[nearby].price+'</div>' +
+                                    '</div>' +
+                                    '</li>');
+                            }
+                            /*refreshImgBox，nearbyImgBox：hover图片动效*/
+                            $('.refreshImgBox .refreshImg .refreshLi .goodImg, .nearbyImgBox .nearbyImg .nearbyLi .goodImg').hover(
+                                function(){
+                                    $(this).siblings('.desc').fadeIn();
+                                },
+                                function(){
+                                    $(this).siblings('.desc').fadeOut();
+                                }
+                            );
+                        }else{
+                            alert(data.msg);
+                        }
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
 
                 $('.headerBox .header .changeUl').append(changeLiStr);
                 $('.headerBox .header .headerLeft .lusaka').html($('.headerBox .header .changeUl .changeLi .changeLiLink').eq(0).html());
@@ -95,8 +142,52 @@ $(function(){
                     $('.headerBox .header .changeUl').hide();
                 });
                 $('.headerBox .header .changeUl .changeLi').click(function () {
+                    var index = $(this).index();
+                    localStorage.setItem('areaId',areaIdArr[index].toString());
+
+                    areaId = localStorage.getItem("areaId");
+                    console.log(areaId);
+                    areaId = areaId.toString();
+                    $.ajax({
+                        type: "get",
+                        url: baseUrl+'/apigateway/getgoods?areaid='+'\"'+areaId+'\"',
+                        success:function(data){
+                            console.log(typeof areaId);
+                            if(data.status == 200){
+                                data = JSON.parse(data.data);
+                                console.log('获取附近商品信息',data);
+                                $('.nearbyImgBox .nearbyImg').empty();
+                                for (var nearby in data){
+                                    $('.nearbyImgBox .nearbyImg').append('' +
+                                        '<li class="nearbyLi">' +
+                                        '<img class="goodImg" src='+baseUrl+'/gimg/'+data[nearby].imgs+'>' +
+                                        '<div class="desc row">' +
+                                        '<div class="col-xs-6 col-sm-6 left">'+data[nearby].title+'</div>' +
+                                        '<div class="col-xs-6 col-sm-6 right">'+data[nearby].price+'</div>' +
+                                        '</div>' +
+                                        '</li>');
+                                }
+                                /*refreshImgBox，nearbyImgBox：hover图片动效*/
+                                $('.refreshImgBox .refreshImg .refreshLi .goodImg, .nearbyImgBox .nearbyImg .nearbyLi .goodImg').hover(
+                                    function(){
+                                        $(this).siblings('.desc').fadeIn();
+                                    },
+                                    function(){
+                                        $(this).siblings('.desc').fadeOut();
+                                    }
+                                );
+                            }else{
+                                alert(data.msg);
+                            }
+                        },
+                        error: function (error) {
+                            console.log(error);
+                        }
+                    });
+
                     $('.headerBox .header .changeUl').hide();
                     $('.headerBox .header .headerLeft .lusaka').text($(this).text());
+
                 });
 
 
