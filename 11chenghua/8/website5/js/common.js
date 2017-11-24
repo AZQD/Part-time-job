5bb1a36300286d9b8a95846061d037edae631a80
刘超杰 <liuchaojie@picooc.com>
2017-11-24 11:50:04 +0800

    task

--- 11chenghua/8/website5/js/common.js
+++ 11chenghua/8/website5/js/common.js
@@ -1,11 +1,8 @@
-$('#header-wrap .header .menu ul li').unbind('click').click(function(event){
-    //$('#header-wrap .header .menu ul li').removeClass('active');
-    //$(event.target).addClass('active');
-    /*alert($(this).attr('item'));
-    if($(this).attr('item')){
-        var index = $(this).attr('item');
-        $('#header-wrap .header .menu ul li').removeClass('active').eq(index).addClass('active');
-    }*/
+$('#header-wrap .header .menu #pcMenu li').unbind('click').click(function(event){
     var index = $(this).index();
-    $('#header-wrap .header .menu ul li').removeClass('active').eq(index).addClass('active');
+    $('#header-wrap .header .menu #pcMenu li').removeClass('active').eq(index).addClass('active');
+});
+$('#header-wrap .header .menu #mobileMenu li').unbind('click').click(function(event){
+    var index = $(this).index();
+    $('#header-wrap .header .menu #mobileMenu li').removeClass('active').eq(index).addClass('active');
 });
