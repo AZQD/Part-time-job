cd644f4cb614f8674abb944489077f40f332e8ff
chaojie <604972395@qq.com>
2017-12-07 22:02:21 +0800

    task

--- 11chenghua/8/website5/js/common.js
+++ 11chenghua/8/website5/js/common.js
@@ -6,3 +6,15 @@ $('#header-wrap .header .menu #mobileMenu li').unbind('click').click(function(ev
     var index = $(this).index();
     $('#header-wrap .header .menu #mobileMenu li').removeClass('active').eq(index).addClass('active');
 });
+$('.main .mid03 .m3b .cont .bar .span1').hover(function(event){
+    $('.main .mid03 .m3b .cont .bar span').removeClass('active');
+    $(this).addClass('active');
+    $('.main .mid03 .m3b .cont .m3bul1').show();
+    $('.main .mid03 .m3b .cont .m3bul2').hide();
+});
+$('.main .mid03 .m3b .cont .bar .span2').hover(function(event){
+    $('.main .mid03 .m3b .cont .bar span').removeClass('active');
+    $(this).addClass('active');
+    $('.main .mid03 .m3b .cont .m3bul1').hide();
+    $('.main .mid03 .m3b .cont .m3bul2').show();
+});
