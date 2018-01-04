6489329cd5676fe4ca69d162e5f9728f81d6cedc
liuchaojie <liuchaojie@58ganji.com>
2018-01-04 20:24:41 +0800

    task

--- 6ZX2/js/index.js
+++ 6ZX2/js/index.js
@@ -118,8 +118,117 @@ $(function(){
         var oWidth=img.width(); //取得图片的实际宽度
         var oHeight=img.height(); //取得图片的实际高度
         img.width(oWidth + size);
-        console.log(oWidth + size);
+        // console.log(oWidth + size);
         //img.height(oHeight + size/oWidth*oHeight);
     }
-    
+
+
+
+    //新增
+    var sortBoxLen = $('.goodsBox .right .sortBox').length;
+    currentSortName();
+    $(window).scroll(function () {
+        currentSortName();
+    });
+
+    //左侧点击切换状态
+    $('.goodsBox .left .goodsSort .sortName').unbind('click').bind('click', function(){
+        var index = $(this).index();
+        $('.goodsBox .left .goodsSort .sortName').removeClass('active').eq(index).addClass('active');
+    });
+
+    //展示当前分类名称
+    function currentSortName() {
+        for(var i = 0; i<sortBoxLen; i++){
+            var scrollSize = $('.goodsBox .right .sortBox').eq(i).offset().top-$(window).scrollTop();
+            if((scrollSize<51)){
+                $('.goodsBox .right .currentName').html($('.goodsBox .right .sortBox').eq(i).children('h3').html());
+                $('.goodsBox .left .goodsSort .sortName').removeClass('active').eq(i).addClass('active');
+            }
+        }
+    }
+
+    //列表加减
+    $('.goodsBox .right .sortBoxWrap .sortBox').delegate('.sortUl .sortLi .partRight .reduce', 'click', function(ev){
+        var index1 = $(this).parents('.sortBox').index();
+        var index2 = $(this).parents('.sortLi').index();
+        console.log(index1);
+        console.log(index2);
+        var $currentSortLi = $('.goodsBox .right .sortBoxWrap .sortBox').eq(index1).children('.sortUl').children('.sortLi').eq(index2);
+        var thisNum = $currentSortLi.children('.partRight').children('.currentCount').html();
+        console.log(thisNum);
+        thisNum--;
+        totalCount--;
+        $('.footerBox .buyBox .left .part .count').html(totalCount);
+        $currentSortLi.children('.partRight').children('.currentCount').html(thisNum);
+        if(thisNum > 0){
+            rotateCart();
+        }else if(thisNum == 0){
+            $currentSortLi.children('.partRight').children('.currentCount, .reduce').hide();
+        }
+    });
+
+    //添加
+    $('.goodsBox .right .sortBoxWrap .sortBox').delegate('.sortUl .sortLi .partRight .add', 'click', function(ev){
+        rotateCart();
+        var index1 = $(this).parents('.sortBox').index();
+        var index2 = $(this).parents('.sortLi').index();
+        console.log(index1);
+        console.log(index2);
+        var $currentSortLi = $('.goodsBox .right .sortBoxWrap .sortBox').eq(index1).children('.sortUl').children('.sortLi').eq(index2);
+        var thisNum = $currentSortLi.children('.partRight').children('.currentCount').html();
+        if(thisNum == 0){
+            $currentSortLi.children('.partRight').children('.currentCount, .reduce').show();
+        }
+        thisNum++;
+        totalCount++;
+        //商品名称
+        var goodName = $currentSortLi.children('.partMiddle').children('.name').html();
+
+        //商品价格
+        var priceStr = '';
+        var originPrice = 0;
+        var currentPrice = 0;
+        if($currentSortLi.children('.partMiddle').children('.price').hasClass('isYouHui')){
+            originPrice = $currentSortLi.children('.partMiddle').children('.price').children('.originPrice').children('.originPriceNum').html();
+            currentPrice = $currentSortLi.children('.partMiddle').children('.price').children('.priceNum').html();
+            priceStr = '<span class="price">\n' +
+    '                        <span class="currentPrice hasYouHui"><span class="currentNum">'+currentPrice+'</span>&nbsp;元</span><br/>\n' +
+    '                        <span class="originPrice">原价<span class="through"><i class="originNum">'+originPrice+'</i>&nbsp;元</span></span>\n' +
+    '                    </span>';
+        }else{
+            currentPrice = $currentSortLi.children('.partMiddle').children('.price').children('.priceNum').html();
+            priceStr = '<span class="price">\n' +
+    '                        <span class="currentPrice"><span class="currentNum">'+currentPrice+'</span>&nbsp;元</span><br/>\n' +
+    '                    </span>'
+        }
+
+
+        $('.footerBox .buyBox .left .part .count').html(totalCount);
+        $currentSortLi.children('.partRight').children('.currentCount').html(thisNum);
+
+
+        console.log(111111,index1+''+index2);
+        if(thisNum == 1){
+            $('.footerBox .listBox .cartList').append(
+                '<li class="cartLi" id='+index1+''+index2+'>\n' +
+    '                <div class="left">'+goodName+'</div>\n' +
+    '                <div class="right">\n'+priceStr+'<img class="reduce" src="image/index/reduce.png" alt="">\n' +
+    '                    <span class="currentCount">'+thisNum+'</span>\n' +
+    '                    <img class="add" src="image/index/add.png" alt="">\n' +
+    '                </div>\n' +
+    '            </li>'
+            );
+        }else{
+            for(var i = 0; i<$('.footerBox .listBox .cartList .cartLi').length; i++){
+                if($('.footerBox .listBox .cartList .cartLi').eq(i).attr('id') == (index1+''+index2)){
+                    $('.footerBox .listBox .cartList .cartLi').eq(i).find('.currentCount').html(thisNum);
+                }
+            }
+            // $('.footerBox .listBox .cartList .cartLi').attr('id').find('.currentCount').html(thisNum);
+        }
+    });
+
+
+
 });
