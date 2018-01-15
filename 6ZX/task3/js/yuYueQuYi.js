63aba525f0c6799ca9abfc37c58b723cc631ed3c
chaojie <604972395@qq.com>
2018-01-16 02:09:29 +0800

    task

--- 6ZX/task3/js/yuYueQuYi.js
+++ 6ZX/task3/js/yuYueQuYi.js
@@ -2,5 +2,6 @@ $(function(){
     $('.wrapper .bottomBtn').unbind('click').bind('click', function(){
         window.location.href = 'myOrder.html';
     });
-    commonPopFun1('2222222222', '1111111', 'a', 'b', '', '');
+    // commonPopFun1('订单金额<span class="price">¥233</span>', '温馨提示', '付款', 'fun');
+    commonPopFun2('您有正在洗护的订单<br/>是否合并订单', '温馨提示', '否', '是', 'fun1', 'fun2');
 });
