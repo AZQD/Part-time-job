afb0500adc06473c35c0372aa08e0e1cd4971eac
chaojie <604972395@qq.com>
2018-01-24 16:30:27 +0800

    task

---
+++ 2方块熊乐园/h5/糖游游/h.youkongwan.com/mbdzz/appData.js
@@ -0,0 +1,14 @@
+var appData = {
+    "headImg":"./image/icon_96.png",
+    "appName":"萌宝大作战",
+    "fen":"4.8分",
+    "downloadNum":"28,000",
+    "size":"29.40MB",
+    "adUrl":"http://static.youkongwan.cn/rqdld/rqdld_1.5.1.apk",
+    "iosUrl":"https://itunes.apple.com/app/id1329885060",
+    "banner":["./image/s1.jpg","./image/s2.jpg","./image/s3.jpg", "./image/s4.jpg", "./image/s5.jpg"],
+    //"banner":["./image/1.jpg","./image/2.jpg","./image/3.jpg"],
+    "desc":"萌宝大作战是一款1v1自动对战竞技、放置游戏，玩家在做好作战准备后就可以匹配全球的玩家进行全自动竞技对战，对战时长不超过15秒。玩家通过收集装备、熔炼升级装备来提升自己的猫咪，当装备套装满足条件时还可激活被动技能！随着猫咪的不断成长，可以匹配到更强的对手，获得更多的奖励！",
+    "versionNum":"2.0.3",
+    "updataDate":"01/12/2017"
+};
